2e5ba61d6f5977e123a6cad009ffb8b9b8b0af3e
Mark T. Holder <mtholder@gmail.com>
2012-05-01 21:22:58 -0500

    removed bogus redirect

--- run_eval2.sh
+++ run_eval2.sh
@@ -3,6 +3,6 @@ parent=$(dirname $0)
 numleaves=$1
 birth=$2
 death=$3
-"${parent}/generate_data.py" "${numleaves}" "${birth}" "${death}" > simtree || exit 1
+"${parent}/generate_data.py" "${numleaves}" "${birth}" "${death}" || exit 1
 paup -n "${parent}/execute.nex" || exit 3
 "${parent}/compare_trees.py" simtree inferred.tre > summary.csv || exit 4
